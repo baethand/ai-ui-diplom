48f274e89156226c129f3171addabe87dbfd97fe
NameSsis <rudenko_lex@mail.ru>
2025-03-16 23:39:03 +0300

    init

---
+++ ai-ui-diplom/src/main/java/ru/mirea/diplom/ai_ui_diplom/AiUiDiplomApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.diplom.ai_ui_diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiUiDiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiUiDiplomApplication.class, args);
+	}
+
+}
